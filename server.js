fe4e8a7dcbf9c740382a523ab29a7c11e4f4d087
Manikandan <manikandan.elumalai@acadaca.com>
2020-02-19 10:40:06 +0530

    port

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // Serve only the static files form the dist directory
@@ -16,4 +18,7 @@ res.sendFile(path.join(__dirname+'/dist/angularapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
